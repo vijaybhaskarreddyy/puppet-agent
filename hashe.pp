d0f013682e3da0d0b369d4d7ab4d44fde3224967
vijaybhaskarreddyy <abhaskarreddy505@gmail.com>
2016-11-17 13:03:16 +0530

    hashes in puppet

---
+++ hashe.pp
@@ -0,0 +1,12 @@
+$fildetails= {
+  'path' => '/tmp/test.txt',
+  'owner' => 'root'
+  'group'=> 'root'
+  'mode'=> '0644'
+  'content' => "testing"
+}
+file { $fildetails['path']:
+  ensure => file,
+  mode => $fildetails['mode']
+  content => $fildetails['content']
+}
